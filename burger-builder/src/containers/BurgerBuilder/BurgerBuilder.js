53dbd1a7000bbf5410f7e3956f75655d0806aced
MounikaKuppuraj <mounikakuppuraj@gmail.com>
2020-01-03 19:54:41 +0530

    "firebase"

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 const INGREDIENT_PRICES={
     meat:5,
     bacon:3.5,
@@ -58,7 +59,23 @@ class BurgerBuilder extends Component {
         this.setState({purchasing:false})
     }
     purchaseContinueHandler=()=>{
-        alert('You Continue!')
+        const order={
+            ingredients:this.state.ingredients,
+            totalPrice:this.state.totalPrice,
+            customer:{
+                name:'Mounika',
+                address:{
+                    street:'TestStreet',
+                    zipcode:600119,
+                    country:'India'
+                },
+                email:'mounikakuppuraj@gmail.com'
+            },
+            delivery:'fastest'
+        }
+        axios.post('/orders.json',order)
+        .then(response=>console.log(response))
+        .catch(error=>console.log(error))
     }
     render() {
         const disabledInfo={...this.state.ingredients}
